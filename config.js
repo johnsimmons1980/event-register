debfc9019ceabd9e6cce78f1b813813de4ab8534
john.simmons <john.simmons@easyjet.com>
2025-06-10 11:44:04 +0100

    Add config.js for register scripts and implement register selector in index.html

---
+++ config.js
@@ -0,0 +1,5 @@
+// config.js
+const REGISTER_SCRIPTS = {
+  Renegades: "https://script.google.com/macros/s/AKfycbzjaPg_XRzTtcl0QOV7Xw98CtZfMYgGslA5NIWETGvBjHYm3wYi3A7rkTrsz2dog5nj9Q/exec",
+  Vengeance: "https://script.google.com/macros/s/AKfycbzthRng7AGk8QhssG6cN0XGYuPb2jT0p3gKlDJbxyPiEGRgfSPtajhRwSuCuLhVKUgy/exec"
+};
